bb410eaefd80ed002135d8c80e327d51a0342bdc
Jan Geschinsky <jan.geschinsky@gmx.de>
2021-04-24 16:15:45 +0200

    erster Commit

---
+++ roboter/meinePins.py
@@ -0,0 +1,31 @@
+MOT_A  = 1
+MOT_B  = 2
+MOT_AB = 3
+#Motoren
+PWMA  = 7
+PWMB  = 12
+A1    = 9
+A2    = 8
+B1    = 10
+B2    = 11
+#Sensoren
+SENSOR_R = 26
+SENSOR_L = 27
+
+ANZ_LS = 2
+BUTTON  = 6
+LED_R   = 1
+LED_G   = 2
+LED_W   = 3
+ANZ_LEDs = 3
+WEISS = 0
+ROT   = 1
+GRUEN = 2
+
+LED_PINS=[1,2,3]
+DATEI="kalibrierWerte.csv"    
+#Display
+SCL = 21
+SDA = 20
+WIDTH  = 128   # oled display width
+HEIGHT = 64    # oled display height
